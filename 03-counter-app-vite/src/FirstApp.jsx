3d56e44987d71df18ae48f96e10719791e81f91f
EDallas <inmsanz89@gmail.com>
2024-03-06 11:03:33 +0000

    Sección 3. Comparación entre Create React App y Vite. Primeros pasos en React (Vite)

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,25 @@
+//import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+export const FirstApp = ({title, subTitle, name }) => {
+  
+  return (
+    <>
+        <h1>{title}</h1>
+        {/* <code>{ JSON.stringify(newMessage) }</code> */}
+        <p>{subTitle}</p>
+        <p>{name}</p>
+    </>
+  )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+}
+
+FirstApp.defaultProps = {
+  name: 'EDallas',
+  subTitle: 'No hay subtitulo',
+  title: 'No hay título',
+}
